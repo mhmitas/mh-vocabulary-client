1b11fe31abed7299f6e06fd88e483b6d88b9c076
Mahfuzul Hoque Miras <155277332+mhmitas@users.noreply.github.com>
2024-08-14 09:47:06 +0600

    Update tailwind.config.js theme config with mhflicks

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,27 +12,40 @@ export default {
       {
         dark: {
           ...require("daisyui/src/theming/themes")["dark"],
-          "base-content": "#efecec"
+          "primary": "#f43f5e",
+          "primary-focus": "#8462f4",
+          "primary-content": "#ffffff",
+          "secondary": "#fff",
+          "secondary-focus": "#fff",
+          "secondary-content": "#000",
+          "accent": "#37cdbe",
+          "accent-focus": "#2daaa3",
+          "accent-content": "#ffffff",
+          "neutral": "#1e293b",
+          "neutral-focus": "#111c39",
+          "neutral-content": "#ffffff",
+          "info": "#2094f3",
+          "info-focus": "#1e87f0",
+          "info-content": "#ffffff",
+          "success": "#009485",
+          "success-focus": "#007965",
+          "success-content": "#ffffff",
+          "error": "#ff0000",
+          "error-focus": "#ff0000",
+          "error-content": "#ffffff",
+          "warning": "#ff9900",
+          "warning-focus": "#e67e00",
+          "warning-content": "#ffffff",
+          "base-100": "#0f172a",
+          "base-200": "#1e293b",
+          // "base-300": "#ebebeb",
+          "base-content": "#ffffff",
         },
-      },
-    ],
+      }
+    ]
   },
   plugins: [require("daisyui")],
 }
 
-/* 
-// light
-"primary": "#1f6feb", // 3b82f6
-"secondary": "#a3e635",
-"accent": "#f43f5e",
-"info": "#38bdf8",
-"success": "#22c55e",
-"warning": "#fbbf24",
-"neutral": "#2a323c",
-"neutral-content": "#ffffff",
-"error": "#ef4444",
-"base-100": "#f0f2f5",
-"base-200": "#ffffff",
-"base-300": "#f0f2f5",
-'base-content': 'black',
-*/
+
+// previous primary color #e11d48
