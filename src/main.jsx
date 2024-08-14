70c9da06f460a8f492764b8574be853b033233d0
Mh Mitas <mahfuzulmitas@gmail.com>
2024-08-14 07:23:44 +0600

    comment out analytics

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { router } from './routes/router.jsx'
 import AuthProvider from './providers/AuthProvider.jsx'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
-import { Analytics } from "@vercel/analytics/react"
+// import { Analytics } from "@vercel/analytics/react"
 
 const queryClient = new QueryClient()
 
@@ -17,7 +17,7 @@ createRoot(document.getElementById('root')).render(
         <RouterProvider router={router} />
       </AuthProvider>
       <Toaster />
-      <Analytics />
+      {/* <Analytics /> */}
     </QueryClientProvider>
   </StrictMode>,
 )
